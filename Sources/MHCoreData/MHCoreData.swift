b02351187dc9eb4922d2fa9118af8638dbb080c9
Martin Hrubý <hrubym@fit.vutbr.cz>
2020-03-22 09:02:03 +0100

    d

--- Sources/MHCoreData/MHCoreData.swift
+++ Sources/MHCoreData/MHCoreData.swift
@@ -1,3 +1,14 @@
-struct MHCoreData {
-    var text = "Hello, World!"
+import Foundation
+import CoreData
+
+//
+extension NSManagedObject {
+    //
+    public func sayHello() -> String {
+        //
+        guard let en = entity.name else { return "Unknown" }
+        
+        //
+        return "Hello from \(en)"
+    }
 }
