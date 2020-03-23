b56061e69cfee8b0d93c11c8127e808d3a9a6918
Martin Hrubý <hrubym@fit.vutbr.cz>
2020-03-23 10:30:09 +0100

    uiv

---
+++ Sources/MHCoreData/General-UIKit-VCs.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by Martin Hruby on 23/03/2020.
+//
+
+import Foundation
+import UIKit
+
+
+///
+///
+///
+public extension UIViewController {
+    //
+    func embedInNav() -> UINavigationController {
+        //
+        UINavigationController(rootViewController: self)
+    }
+}
+
+///
+///
+///
+extension UINavigationController {
+    //
+    func add(tabBarItem title: String, tag: Int, image: UIImage? = nil)
+    {
+        //
+        tabBarItem = UITabBarItem(title: title, image: image, tag: tag)
+    }
+}
+
---
+++ Sources/MHCoreData/General-UIKit-Views.swift
@@ -0,0 +1,138 @@
+//
+//  File.swift
+//  
+//
+//  Created by Martin Hruby on 23/03/2020.
+//
+
+import Foundation
+import UIKit
+
+//
+extension UIView {
+    //
+    @available(iOS 9.0, *)
+    func baseStack(_ pin: CGFloat = 20) -> UIStackView {
+        //
+        let hstack = UIStackView()
+        
+        //
+        addSubview(hstack)
+        
+        //
+        hstack.translatesAutoresizingMaskIntoConstraints = false
+        hstack.axis = .horizontal
+        
+        //
+        hstack.leftAnchor.constraint(equalTo: leftAnchor,
+                                     constant: pin).isActive = true
+        hstack.rightAnchor.constraint(equalTo: rightAnchor,
+                                      constant: -pin).isActive = true
+        hstack.topAnchor.constraint(equalTo: topAnchor,
+                                    constant: pin).isActive = true
+        hstack.bottomAnchor.constraint(equalTo: bottomAnchor,
+                                       constant: -pin).isActive = true
+        
+        //
+        return hstack
+    }
+}
+
+//
+@available(iOS 9.0, *)
+extension UIStackView {
+    //
+    func addSpacing(_ sp: CGFloat = 5) {
+        //
+        spacing = sp
+    }
+    
+    //
+    func minHeight(_ val: CGFloat) {
+        //
+        heightAnchor.constraint(greaterThanOrEqualToConstant: val).isActive = true
+    }
+    
+    //
+    func hstack() -> UIStackView {
+        //
+        let h = UIStackView()
+        
+        //
+        h.axis = .horizontal
+        h.translatesAutoresizingMaskIntoConstraints = false
+        
+        //
+        addArrangedSubview(h)
+        
+        //
+        return h
+    }
+    
+    //
+    func vstack() -> UIStackView {
+        //
+        let h = UIStackView()
+        
+        //
+        h.axis = .vertical
+        h.translatesAutoresizingMaskIntoConstraints = false
+        
+        //
+        addArrangedSubview(h)
+        
+        //
+        return h
+    }
+    
+    //
+    func add(inView: UIView) {
+        //
+        addArrangedSubview(inView)
+        
+        //
+        inView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    //
+    func add(label cont: String = "") -> UILabel
+    {
+        let ltext = UILabel()
+        
+        //
+        addArrangedSubview(ltext)
+        
+        //
+        ltext.translatesAutoresizingMaskIntoConstraints = false
+        ltext.textAlignment = .left
+        ltext.text = cont
+        
+        //
+        return ltext
+    }
+    
+    //
+    func add(image cont: UIImage?, size: CGSize?) -> UIImageView
+    {
+        let ima = UIImageView(image: cont)
+        
+        //
+        addArrangedSubview(ima)
+        
+        //
+        ima.translatesAutoresizingMaskIntoConstraints = false
+        
+        //
+        if let _size = size {
+            //
+            ima.widthAnchor.constraint(equalToConstant: _size.width).isActive = true
+            ima.heightAnchor.constraint(equalToConstant: _size.height).isActive = true
+        }
+        
+        //
+        return ima
+    }
+}
+
+
+
